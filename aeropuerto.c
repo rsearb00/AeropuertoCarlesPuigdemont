21db4e4f9b2219db377bd92427f8b4edd3918164
mpujoh00 <mpujoh00@estudiantes.unileon.es>
2018-12-17 11:50:15 +0100

    base

--- aeropuerto.c
+++ aeropuerto.c
@@ -6,7 +6,61 @@
 #include <signal.h>
 #include <sys/wait.h>
 
+
+/*semáforos*/
+
+/*variables de condición*/
+
+/*contador de usuarios*/
+int contadorUsuarios;
+
+/*lista usuarios*/
+
+struct usuario {
+
+	int idUsuario;
+	int facturado;
+	int atendido;
+	int tipo;
+};
+
+struct usuario usuarios[10];
+
+/*usuarios en control*/
+
+/*fichero log*/
+
+
+
 int main (int argc, char *argv[]){
 
-	
+	if (signal(SIGUSR1, nuevoUsuario) == SIG_ERR) {
+
+		perror("Llamada a signal.");
+		exit(-1);
+	}
+
+	if (signal(SIGUSR2, nuevoUsuario) == SIG_ERR) {
+
+		perror("Llamada a signal.");
+		exit(-1);
+	}
+
 }
+
+void nuevoUsuario(int s) {
+
+	if (s == SIGUSR1) {
+		tipo = 0;
+		printf("Soy un usuario normal.");
+	}
+
+	if (s == SIGUSR2) {
+		tipo = 1;
+		printf("Soy un usuario vip.");
+	}
+
+}
+
+
+
