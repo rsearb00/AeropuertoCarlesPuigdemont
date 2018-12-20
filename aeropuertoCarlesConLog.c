f2fc8b7f688a76c18777e6ee8d11b4e1fe719b52
rsearb00 <36696319+rsearb00@users.noreply.github.com>
2018-12-20 18:06:57 +0100

    Actualizado, ahora sí

--- aeropuertoCarlesConLog.c
+++ aeropuertoCarlesConLog.c
@@ -5,7 +5,7 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
+#include <time.h>
 /*contador de usuarios*/
 int contadorUsuarios;
 
@@ -19,10 +19,12 @@ struct usuario{
 };
 
 struct usuario usuarios[10];
-
-
+/*fichero log*/
+FILE *logFile;
 /*declaracion de funciones*/
 void nuevoUsuario(int sig);
+//Función para escribir en el log
+void writeLogMessage(char *id, char *msg);
 
 int main(int argc, char const *argv[])
 {
@@ -55,3 +57,22 @@ void nuevoUsuario(int sig){
             exit(0);
     }
 }
+
+//Función para escribir en el log
+void writeLogMessage(char *id, char *msg){
+	//Calculamos la hora
+	time_t now = time(0);
+	struct tm *tlocal = localtime(&now);
+	char stnow[26];
+	strftime(stnow, 26, "%d/%m/%y %H:%M:%S", tlocal);
+	
+	//Escribimos en el log
+	/* La "a" significa: que si el fichero no existe lo crea para escribir en él 
+	y lo deja abierto, por defecto, en modo texto, y si ya existe permite añadir 
+	mas datos al final de este respetando sus datos anteriores.
+	*/
+	logFile=fopen("logFile.txt", "a");
+	fprintf(logFile, "[%s] %s: %s:\n", stnow, id, msg);
+	fclose(logFile);
+}
+
